628b6438f55fa1bf95cf9277bf4b74e2b9c1bef5
akahaba <amatsura@gmail.com>
2017-07-08 18:08:43 +0900

    first commit

--- linebot.php
+++ linebot.php
@@ -46,6 +46,14 @@ if(strpos($message_text,'確認') !== false){
 				$res = pg_query( $pg_conn, $sqlcmd);
 				//var_dump($res);
 
+			$resultScore ="";
+			for ($i = 0 ; $i < pg_num_rows($res) ; $i++){
+			    $rows = pg_fetch_array($res, NULL, PGSQL_ASSOC);
+			    $resultScore=$resultScore.$rows['player']."\t".$rows['Sum(totalpoints)']."\n";
+			}
+
+
+
 				$db_message = "クエリ実行できました";
 				
 			} else {

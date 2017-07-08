86e48fff3e8307d8f823d1be2e6d0541487b9cdb
akahaba <amatsura@gmail.com>
2017-07-08 15:27:17 +0900

    first commit

--- scorekeisan.php
+++ scorekeisan.php
@@ -31,14 +31,14 @@ function return_score($message_text) {
 
 	//最後の行はコマンド　登録　修正　削除＋ゲーム番号
 	$cmdstr = get_last_key($gameResult);
-	//$gameNm = get_last_value($gameResult);
+	$gameNm = get_last_value($gameResult);
 	
 	if($cmdstr=='登録') {
-		$return_message_text = "登録モードです\n"."ゲーム番号";
+		$return_message_text = "登録モードです\n"."ゲーム番号".$gameNm;
 	} elseif($cmdstr=='修正') {
-		$return_message_text = "修正モードです\n"."ゲーム番号";
+		$return_message_text = "修正モードです\n"."ゲーム番号".$gameNm;
 	} elseif($cmdstr=='削除') {
-		$return_message_text = "削除モードです\n"."ゲーム番号";
+		$return_message_text = "削除モードです\n"."ゲーム番号".$gameNm;
 	} else {	//表示モード
 
 		//array_pop($gameResult);

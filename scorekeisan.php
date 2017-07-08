87982616f0a7abdb8a54fd0ce4014d423081d3db
akahaba <amatsura@gmail.com>
2017-07-08 16:38:57 +0900

    first commit

--- scorekeisan.php
+++ scorekeisan.php
@@ -71,7 +71,7 @@ function return_score($message_text)
 		//$result = array_merge($arrGame,"name"=>$key);
 		//$arrGame += array($key=>$arrPlayerResult);
 
-		$sql[$i]= $sql_str. $key .",". $gameResult[$key].",".$scoringPoints[$key]).",". $umaPoints[$i].",".$totalPoints[$key].");";
+		//$sql[$i]= $sql_str. $key .",". $gameResult[$key].",".$scoringPoints[$key]).",". $umaPoints[$i].",".$totalPoints[$key].");";
 		$return_message_text = $key . "さんは" . $scoringPoints[$key]."\t".$uma[$i]."\t".$totalPoints[$key]."\n".$return_message_text;
 		$i = $i-1;
 		}
@@ -83,7 +83,7 @@ function return_score($message_text)
 
 		if($cmdstr=='登録') {
 			$return_message_text = $return_message_text."\n登録モードです\n"."ゲーム番号".$gameNm;
-			$return_message_text = $return_message_text.$sql[0];
+			$return_message_text = $return_message_text.$sql_str;
 		
 		} elseif($cmdstr=='修正') {
 			$return_message_text =  $return_message_text."\n修正モードです\n"."ゲーム番号".$gameNm;

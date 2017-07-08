f90d3b484fbf98e94508ad93ed40f572e6a8cc63
akahaba <amatsura@gmail.com>
2017-07-08 18:41:54 +0900

    first commit

--- linebot.php
+++ linebot.php
@@ -33,7 +33,7 @@ if(strpos($message_text,'確認') !== false){
 	$return_message_text = "現在の結果だよ！";
 //	$return_message_text = record_score();
 
-	$sqlcmd="SELECT player, Sum(totalpoints) As total FROM mjtable GROUP BY player order by total desc;";
+	$sqlcmd="SELECT player, Sum(scoringpoints) As pt,Sum(umapoints) As uma,Sum(totalpoints) As total FROM mjtable GROUP BY player order by total desc;";
 
 		//DB接続
 		// 各種パラメータを指定して接続
@@ -49,7 +49,7 @@ if(strpos($message_text,'確認') !== false){
 			$resultScore ="";
 			for ($i = 0 ; $i < pg_num_rows($res) ; $i++){
 			    $rows = pg_fetch_array($res, NULL, PGSQL_ASSOC);
-			    $resultScore=$resultScore.$rows['player']."\t".$rows['total']."\n";
+			    $resultScore=$resultScore.$rows['player']."\t".$rows['pt']."\t".$rows['uma']."\t".$rows['total']."\n";
 			}
 
 

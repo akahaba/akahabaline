e51237b0427ead79c0f17c78e0b82675d3114758
akahaba <amatsura@gmail.com>
2017-07-08 17:03:17 +0900

    first commit

--- scorekeisan.php
+++ scorekeisan.php
@@ -44,7 +44,7 @@ function return_score($message_text)
 	//$sql = array();
 	
 	$date_s=(string)date("Ymd");
-	//$endTime_s=date("H:i:s", strtotime('+9 hour');
+	$endTime_s=(string)date("H:i:s", strtotime('+9 hour');
 	$player_s="";
 	$score_s=0;
 	$rank_s=0;
@@ -89,7 +89,7 @@ function return_score($message_text)
 
 		//.$date_s."','".$endTime_s."','"
 		//$sql[$i]= $sql_str. $key .",". $gameResult[$key].",".$scoringPoints[$key]).",". $umaPoints[$i].",".$totalPoints[$key].");";
-		$sql_test=$sql_str."'".$player_s."',".$score_s.",".$rank_s.",".$scoringPoints_s.",".$umaPoints_s.",".$totalPoints_s.");";
+		$sql_test=$sql_str.$date_s."','".$endTime_s."','"."'".$player_s."',".$score_s.",".$rank_s.",".$scoringPoints_s.",".$umaPoints_s.",".$totalPoints_s.");";
 		$return_message_text = $key . "さんは" . $scoringPoints[$key]."\t".$uma[$i]."\t".$totalPoints[$key]."\n".$return_message_text;
 		$i = $i-1;
 		}

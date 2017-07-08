245231377c8aa42b0aad6ad0225c6618cde93396
akahaba <amatsura@gmail.com>
2017-07-08 10:28:26 +0900

    make it better

--- scorekeisan.php
+++ scorekeisan.php
@@ -53,13 +53,13 @@ function return_score($message_text) {
 		//JSON用arrayへの代入
 		$arrPlayerResult = array("name"=>$key,"rank"=>($i+1),"score"=>$gameResult[$key],"scoringPoints"=>$scoringPoints[$key],"umaPoints"=>$umaPoints[$i],"totalPoints"=>$totalPoints[$key]);
 		$arrPlayerResult = json_encode($arrPlayerResult);
-		file_put_contents("test.json" , $arrPlayerResult);
+		//file_put_contents("test.json" , $arrPlayerResult);
 
 		$return_message_text = $key . "さんは" . $scoringPoints[$key]."\t".$uma[$i]."\t".$totalPoints[$key]."\n".$return_message_text;
 		$i = $i-1;
 		}
 
-		$return_message_text = $return_message_text. "\nみなさん頑張ってくださいね～";
+		$return_message_text = $return_message_text. "\nみなさん頑張ってくださいね～".$arrPlayerResult;
 
 	return $return_message_text;
 

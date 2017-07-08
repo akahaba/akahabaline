6205a304e80a5c68c51d4d4ce3816b386a5aa6b4
akahaba <amatsura@gmail.com>
2017-07-08 21:09:19 +0900

    first commit

--- linebot.php
+++ linebot.php
@@ -90,7 +90,7 @@ $sqlrollup = "select handnumber,sum(case player when '朝倉' then totalpoints e
 			$resultScore ="";
 			for ($i = 0 ; $i < pg_num_rows($res) ; $i++){
 			    $rows = pg_fetch_array($res, NULL,PGSQL_NUM );
-			    $resultScore=$resultScore.str_pad($rows[0], 4, " ", STR_PAD_LEFT)."\t".str_pad($rows[1], 4, " ", STR_PAD_LEFT)."\t".str_pad($rows[2], 4, " ", STR_PAD_LEFT)."\t".str_pad($rows[3], 4, " ", STR_PAD_LEFT)."\t".str_pad($rows[4], 4, " ", STR_PAD_LEFT)."\n";
+			    $resultScore=$resultScore.str_pad($rows[0], 4, " ", STR_PAD_LEFT).str_pad($rows[1], 6, " ", STR_PAD_LEFT).str_pad($rows[2], 6, " ", STR_PAD_LEFT).str_pad($rows[3], 6, " ", STR_PAD_LEFT).str_pad($rows[4], 6, " ", STR_PAD_LEFT)."\n";
 			}
 
 				$db_message = "クエリ実行できました";

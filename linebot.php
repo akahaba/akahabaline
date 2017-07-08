66ba9c67643fc3b0184bbc6948691070c0ad8630
akahaba <amatsura@gmail.com>
2017-07-08 18:09:24 +0900

    first commit

--- linebot.php
+++ linebot.php
@@ -63,7 +63,7 @@ if(strpos($message_text,'確認') !== false){
 			// データベースとの接続を切断
 			pg_close($pg_conn);
 
-	$return_message_text=$return_message_text."\n\n".$res;
+	$return_message_text=$return_message_text."\n\n".$resultScore;
 
 } else {
   //messageの先頭に'確認'が含まれていない場合

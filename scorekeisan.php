a501f6f530d90a7cda01e6d5ef111bc063ca1258
akahaba <amatsura@gmail.com>
2017-07-09 16:03:09 +0900

    make it better

--- scorekeisan.php
+++ scorekeisan.php
@@ -161,7 +161,7 @@ function return_score($message_text)
 			$return_message_text = $return_message_text."\n".$db_message;
 		
 		} elseif($cmdstr=='修正') {
-			$return_message_text =  $return_message_text."\n修正モードです\n"."ゲーム番号".$gameNm."\n".$UpdRows."件更新";
+			$return_message_text =  $return_message_text."\n修正モードです\n"."ゲーム番号".$gameNm."\n".$db_message;
 
 		//DB接続
 		// 各種パラメータを指定して接続

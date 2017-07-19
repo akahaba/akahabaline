a7fa49efb8c14c927d530b84f44cbc7c38b2515d
akahaba <amatsura@gmail.com>
2017-07-19 20:07:08 +0900

    make it better

--- scorekeisan.php
+++ scorekeisan.php
@@ -114,7 +114,7 @@ function return_score($message_text)
 		$rank_s=$i+1;
 		$scoringPoints_s=intval($scoringPoints[$key]);
 		$umaPoints_s=intval($umaPoints[$i]);
-		$totalPoints_s=intval($totalPoints[$key]-$badai[$i]); //場代調整修正2017.07.20
+		$totalPoints_s=intval($totalPoints[$key]); //場代調整修正2017.07.20
 		$tobihakoPoints_s=intval($gameResultTobi[$key]);
 
 		//insert登録の場合のSQL文

4782f79b52ffefb8cb5cf07621a10db9f3aa8fea
Katsuhiro Amatsura <amatsura@gmail.com>
2017-08-17 15:50:16 +0000

    Pushed from Dropbox

--- scorekeisan_gotto2.php
+++ scorekeisan_gotto2.php
@@ -97,7 +97,7 @@ function return_score($message_text)
 		$basePoints[$key] = ($gameResult[$key] - 30000)/1000;
 		if($basePoints[$key]<0){
 			$oka = $oka + ceil($basePoints[$key]);
-			$scoringPoints[$key] = ceil($basePoints[$key]);
+			$scoringPoints[$key] = ceil($basePoints[$key])-$badai[$i]; //場代調整修正2017.08.18
 		} else {
 			$oka = $oka + floor($basePoints[$key]);
 			if($i==0){

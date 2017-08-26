8881f5aeaf6e8612690bd8ab010845f36914b3ac
Katsuhiro Amatsura <amatsura@gmail.com>
2017-08-26 16:17:32 +0000

    Pushed from Dropbox

--- mj.php
+++ mj.php
@@ -1,13 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-    "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-<title>長重雀の会</title>
-</head>
-<body>
-長重雀の会のサイト　工事中
-
 <?php
   /* ### "pg_fetch_all()"によるデータ取得 ### */
 
@@ -48,7 +38,13 @@ pg_close($pg_conn);
 
 echo $db_message;
 ?>
-
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<title>長重雀の会</title>
+</head>
+<body>
+長重雀の会のサイト　工事中
 
 </body>
 </html>

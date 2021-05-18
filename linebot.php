d1932b58aaccf1f3c08cf24c4ca370c4a060139d
akahaba <amatsura@gmail.com>
2021-05-18 15:21:56 +0900

    test0514

--- linebot.php
+++ linebot.php
@@ -384,10 +384,13 @@ if(strpos($message_text,'確認') !== false){
 			pg_close($pg_conn);
 
 
+			preg_match('/設定/', $message_text, $matches);
+			$gamesetting = $matches[1];
+
 			//ゲーム数０の切り分け
 			if($val>0) {
 			//$return_message_text=$return_message_text."\n".mb_substr($message_text, 3, NULL, "UTF-8")."\n"."ゲーム設定値\nレート".$val[0]."\nウマ".$val[1]."-".$val[2];
-			$return_message_text=$return_message_text."\n"."\n"."ゲーム設定値\nレート".$val[0]."\nウマ".$val[1]."-".$val[2];
+			$return_message_text=$return_message_text."\n".$gamesetting."\n"."ゲーム設定値\nレート".$val[0]."\nウマ".$val[1]."-".$val[2];
 			} else {
 			$return_message_text=$return_message_text."\n本日、記録されているゲーム結果はありません";
 			}

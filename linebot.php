d186ffd4c2aa4e36a86c662f1ef0a27c127b9034
akahaba <amatsura@gmail.com>
2021-05-18 10:17:02 +0900

    test0514

--- linebot.php
+++ linebot.php
@@ -22,7 +22,7 @@ $message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッ�
 $message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
 
 //$date_s=(string)date("Ymd");
-$date_s="20210515"; //デバッグ用
+$date_s="20210514"; //デバッグ用
 //精算レート 点５->50 点ピン->100
 $ratevalue=100;
 
